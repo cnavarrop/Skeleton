a31477938b880ed23401fa7604f39587f6d44306
cnavarrop <cristiannavarro.palma@gmail.com>
2022-10-22 17:28:37 -0400

    Pruebas

--- ImplementacionTestlink/src/test/java/Utilities/BaseTest.java
+++ ImplementacionTestlink/src/test/java/Utilities/BaseTest.java
@@ -7,29 +7,18 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.file.Path;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-import org.openqa.selenium.Platform;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.devtools.v102.browser.Browser;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxDriver.Capability;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
-import bsh.Capabilities;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 
