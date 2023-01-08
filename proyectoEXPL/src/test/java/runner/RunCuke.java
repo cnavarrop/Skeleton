10a72b3d29c0d2dbd9616bb885657a426cc37be3
cnavarrop <cristiannavarro.palma@gmail.com>
2023-01-08 16:24:43 -0300

    commit por uso de VSCODE

--- proyectoEXPL/src/test/java/runner/RunCuke.java
+++ proyectoEXPL/src/test/java/runner/RunCuke.java
@@ -3,7 +3,10 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = ".\\src\\test\\resources\\features", glue = {"steps"}, monochrome = true, tags = "@Test")
+@CucumberOptions(features = ".\\src\\test\\resources\\features", glue = {
+        "steps" }, monochrome = true, tags = "@Test", plugin = { "html:target/cucumber-reports/report.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
+
 
 public class RunCuke extends AbstractTestNGCucumberTests{
 
