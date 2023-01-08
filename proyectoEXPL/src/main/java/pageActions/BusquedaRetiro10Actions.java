2e22727d7855b9b8bb523757f732514eadb8baf9
cnavarrop <cristiannavarro.palma@gmail.com>
2023-01-07 21:09:51 -0300

    finalizacion de func. buscar retiro 10%
    se agrega comparacion de textos en if

--- proyectoEXPL/src/main/java/pageActions/BusquedaRetiro10Actions.java
+++ proyectoEXPL/src/main/java/pageActions/BusquedaRetiro10Actions.java
@@ -1,33 +1,79 @@
 package pageActions;
 
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import pageLocators.Retiro10Locators;
 import utils.SeleniumDriver;
 
 public class BusquedaRetiro10Actions {
 	Retiro10Locators retiro10Locators = null;
-	
-	public BusquedaRetiro10Actions()
-	{
+	String rut = "";
+
+	public BusquedaRetiro10Actions() {
 		this.retiro10Locators = new Retiro10Locators();
 		PageFactory.initElements(SeleniumDriver.getDriver(), retiro10Locators);
 	}
-	
-	public void ingresarRut(String rut)
-	{
-		SeleniumDriver.getWait().until(ExpectedConditions.elementToBeClickable(retiro10Locators.txtBoxBuscarRut));
-		retiro10Locators.txtBoxBuscarRut.sendKeys(rut);
+
+	public void ingresarRut(String rut) {
+		this.rut = rut;
+		SeleniumDriver.getDriver().switchTo().frame(0);
+		retiro10Locators.txtRutCss.click();
+		retiro10Locators.txtRutCss.sendKeys(this.rut);
 	}
-	
+
 	public void clickBtnBuscar() {
-		
+
 		retiro10Locators.btnBuscar.click();
-		
+
 	}
+
 	public void clickBtnSalir() {
+
+		retiro10Locators.btnSalir.click();
+	}
+
+	public boolean validaGrilla() {
+		String mensaje = "No hay registros para mostrar";
+		String existe = "";
+		try {
+			existe = retiro10Locators.mensajeAlerta.getText();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	
-	retiro10Locators.btnSalir.click();
+		if (existe.equalsIgnoreCase(mensaje)) {
+			retiro10Locators.btnSalirAlerta.click();
+			return false;
+
+		} else {
+
+			String rutGrilla = formateoRut(retiro10Locators.rutSuscriptor.getText());
+
+			if (rutGrilla.equals(rut)) {
+				retiro10Locators.btnSalirGrilla.click();
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+	}
+
+	public String formateoRut(String rut) {
+		String rutForm = "";
+		String[] datos = rut.split("\\.");
+		for (String string : datos) {
+			rutForm += string;
+		}
+
+		return rutForm.trim();
+
 	}
 }
--- proyectoEXPL/src/main/java/pageLocators/Retiro10Locators.java
+++ proyectoEXPL/src/main/java/pageLocators/Retiro10Locators.java
@@ -6,11 +6,27 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class Retiro10Locators {
-	@FindBy(how = How.XPATH, using = "//*[@id=\"id_sc_field_rut\"]")
+	@FindBy(how = How.XPATH, using = "//*[@id=\'id_sc_field_rut\']")
 	public WebElement txtBoxBuscarRut;
+	
 	@FindBy(how = How.XPATH, using ="//*[@id=\"sub_form_b\"]/span" )
 	public WebElement btnBuscar;
+	
 	@FindBy(how= How.XPATH, using = "//*[@id=\"sc_Salir_bot\"]/span")
 	public WebElement btnSalir;
-
+	
+	@FindBy(how = How.CSS, using = "#id_sc_field_rut")
+	public WebElement txtRutCss;
+	
+	@FindBy(how = How.XPATH, using = "//td[@id='sc_grid_body']")
+	public WebElement mensajeAlerta; 	
+	
+	@FindBy(how = How.XPATH, using = "//*[@id=\"sai_top\"]/span")
+	public WebElement btnSalirAlerta;
+	
+	@FindBy(how = How.XPATH, using = "//*[@id=\"sai_top\"]/span")
+	public WebElement btnSalirGrilla;
+	
+	@FindBy(how = How.XPATH, using = "//*[@id=\"id_sc_field_rut_suscriptor_1\"]")
+	public WebElement rutSuscriptor;
 }
